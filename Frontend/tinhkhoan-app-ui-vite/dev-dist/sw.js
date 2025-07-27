edc6836b2c2f52cedd6d7199193fdec2a513ff31
Nguyen Dat <nguyendat@tinhkhoan.local>
2025-07-27 10:30:15 +0700

    Final commit: Full project restart completed, RR01 implementation ready

--- Frontend/tinhkhoan-app-ui-vite/dev-dist/sw.js
+++ Frontend/tinhkhoan-app-ui-vite/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3li485qa0ig"
+    "revision": "0.butk0vmn45o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
