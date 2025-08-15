0e3ddbd01a7662385359d0d077be86592be06c0d
Nguyen Dat <nguyendat@tinhkhoan.local>
2025-08-15 08:22:42 +0700

    Update PWA service worker
    - Update dev-dist/sw.js for latest PWA cache configuration
    - Cleaned up all empty script files from backend directory

--- Frontend/tinhkhoan-app-ui-vite/dev-dist/sw.js
+++ Frontend/tinhkhoan-app-ui-vite/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ds44a9bgk9g"
+    "revision": "0.5i7ad25p0g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
